1f18a024bba2316de0a40b2c941210182eee5dc9
stormytuna <stormytuna@outlook.com>
2022-11-14 09:08:03 +0000

    Let relics and pets drop in classic

--- Common/GlobalNPCs/NPCLootGlobalNPC.cs
+++ Common/GlobalNPCs/NPCLootGlobalNPC.cs
@@ -1,4 +1,5 @@
 using MasterModeDropsInExpertMode.Common.Configs;
+using MasterModeDropsInExpertMode.Common.Systems;
 using Terraria;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
@@ -10,22 +11,40 @@ namespace MasterModeDropsInExpertMode.Common.GlobalNPCs {
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot) {
             foreach (var rule in npcLoot.Get()) {
                 // Should get most relics
-                if (rule is ItemDropWithConditionRule withConditionRule && withConditionRule.condition is Conditions.IsMasterMode && ServerConfig.Instance.DropRelics) {
+                if (rule is ItemDropWithConditionRule withConditionRule && withConditionRule.condition is Conditions.IsMasterMode) {
                     if (ItemID.Search.TryGetName(withConditionRule.itemId, out string name)) {
                         if (!ServerConfig.Instance.MasterToExpertBlacklist.Contains(new ItemDefinition(name))) {
-                            withConditionRule.condition = new Conditions.IsExpert();
+                            if (ServerConfig.Instance.DropRelicsClassic) {
+                                withConditionRule.condition = new Conditions.NotMasterMode();
+                            }
+
+                            else if (ServerConfig.Instance.DropRelicsExpert) {
+                                withConditionRule.condition = new Conditions.IsExpert();
+                            }
+
+                            ArraySystem.RegisterNewRelic(withConditionRule.itemId);
                         }
                     }
                 }
 
                 // Should get most pets
-                if (rule is DropBasedOnMasterMode masterDrop && masterDrop.ruleForMasterMode is DropPerPlayerOnThePlayer dropPerPlayer && ServerConfig.Instance.DropPets) {
+                if (rule is DropBasedOnMasterMode masterDrop && masterDrop.ruleForMasterMode is DropPerPlayerOnThePlayer dropPerPlayer) {
                     if (ItemID.Search.TryGetName(dropPerPlayer.itemId, out string name)) {
                         if (!ServerConfig.Instance.MasterToExpertBlacklist.Contains(new ItemDefinition(name))) {
                             // Add and remove since the original rule is a DropBasedOnMasterMode drop
                             int denom = ServerConfig.Instance.GuaranteedPets ? 1 : dropPerPlayer.chanceDenominator;
-                            npcLoot.Add(new DropPerPlayerOnThePlayer(dropPerPlayer.itemId, denom, dropPerPlayer.amountDroppedMinimum, dropPerPlayer.amountDroppedMaximum, new Conditions.IsExpert()));
-                            npcLoot.Remove(dropPerPlayer);
+
+                            if (ServerConfig.Instance.DropPetsClassic) {
+                                npcLoot.Add(new DropPerPlayerOnThePlayer(dropPerPlayer.itemId, denom, dropPerPlayer.amountDroppedMinimum, dropPerPlayer.amountDroppedMaximum, new Conditions.NotMasterMode()));
+                                npcLoot.Remove(dropPerPlayer);
+                            }
+
+                            else if (ServerConfig.Instance.DropPetsExpert) {
+                                npcLoot.Add(new DropPerPlayerOnThePlayer(dropPerPlayer.itemId, denom, dropPerPlayer.amountDroppedMinimum, dropPerPlayer.amountDroppedMaximum, new Conditions.IsExpert()));
+                                npcLoot.Remove(dropPerPlayer);
+                            }
+
+                            ArraySystem.RegisterNewPet(dropPerPlayer.itemId);
                         }
                     }
                 }
@@ -33,22 +52,41 @@ namespace MasterModeDropsInExpertMode.Common.GlobalNPCs {
                 // Should do the twins
                 if (rule is LeadingConditionRule missingTwin && missingTwin.condition is Conditions.MissingTwin) {
                     foreach (var chain in missingTwin.ChainedRules) {
-                        if (chain.RuleToChain is ItemDropWithConditionRule twinsWithConditionRule && twinsWithConditionRule.condition is Conditions.IsMasterMode && ServerConfig.Instance.DropRelics) {
+                        if (chain.RuleToChain is ItemDropWithConditionRule twinsWithConditionRule && twinsWithConditionRule.condition is Conditions.IsMasterMode) {
                             if (ItemID.Search.TryGetName(twinsWithConditionRule.itemId, out string name)) {
                                 if (!ServerConfig.Instance.MasterToExpertBlacklist.Contains(new ItemDefinition(name))) {
-                                    twinsWithConditionRule.condition = new Conditions.IsExpert();
+                                    if (ServerConfig.Instance.DropRelicsClassic) {
+                                        twinsWithConditionRule.condition = new Conditions.NotMasterMode();
+                                    }
+
+                                    else if (ServerConfig.Instance.DropRelicsExpert) {
+                                        twinsWithConditionRule.condition = new Conditions.IsExpert();
+                                    }
+
+                                    ArraySystem.RegisterNewRelic(twinsWithConditionRule.itemId);
                                 }
                             }
                         }
-                        if (chain.RuleToChain is DropBasedOnMasterMode twinsMasterDrop && twinsMasterDrop.ruleForMasterMode is DropPerPlayerOnThePlayer twinsDrop && twinsDrop.condition is Conditions.IsMasterMode && ServerConfig.Instance.DropPets) {
+                        if (chain.RuleToChain is DropBasedOnMasterMode twinsMasterDrop && twinsMasterDrop.ruleForMasterMode is DropPerPlayerOnThePlayer twinsDrop && twinsDrop.condition is Conditions.IsMasterMode) {
                             if (ItemID.Search.TryGetName(twinsDrop.itemId, out string name)) {
                                 if (!ServerConfig.Instance.MasterToExpertBlacklist.Contains(new ItemDefinition(name))) {
                                     // Add and remove since the original rule is a DropBasedOnMasterMode drop
                                     int denom = ServerConfig.Instance.GuaranteedPets ? 1 : twinsDrop.chanceDenominator;
                                     LeadingConditionRule newRule = new LeadingConditionRule(new Conditions.MissingTwin());
-                                    newRule.OnSuccess(new DropPerPlayerOnThePlayer(twinsDrop.itemId, denom, twinsDrop.amountDroppedMinimum, twinsDrop.amountDroppedMaximum, new Conditions.IsExpert()));
-                                    npcLoot.Add(newRule);
-                                    npcLoot.Remove(twinsDrop);
+
+                                    if (ServerConfig.Instance.DropPetsClassic) {
+                                        newRule.OnSuccess(new DropPerPlayerOnThePlayer(twinsDrop.itemId, denom, twinsDrop.amountDroppedMinimum, twinsDrop.amountDroppedMaximum, new Conditions.NotMasterMode()));
+                                        npcLoot.Add(newRule);
+                                        npcLoot.Remove(twinsDrop);
+                                    }
+
+                                    else if (ServerConfig.Instance.DropPetsExpert) {
+                                        newRule.OnSuccess(new DropPerPlayerOnThePlayer(twinsDrop.itemId, denom, twinsDrop.amountDroppedMinimum, twinsDrop.amountDroppedMaximum, new Conditions.IsExpert()));
+                                        npcLoot.Add(newRule);
+                                        npcLoot.Remove(twinsDrop);
+                                    }
+
+                                    ArraySystem.RegisterNewPet(twinsDrop.itemId);
                                 }
                             }
                         }
@@ -58,22 +96,41 @@ namespace MasterModeDropsInExpertMode.Common.GlobalNPCs {
                 // Should do the eater of worlds
                 if (rule is LeadingConditionRule eaterOfWorlds && eaterOfWorlds.condition is Conditions.LegacyHack_IsABoss) {
                     foreach (var chain in eaterOfWorlds.ChainedRules) {
-                        if (chain.RuleToChain is ItemDropWithConditionRule eaterWithConditionRule && eaterWithConditionRule.condition is Conditions.IsMasterMode && ServerConfig.Instance.DropRelics) {
+                        if (chain.RuleToChain is ItemDropWithConditionRule eaterWithConditionRule && eaterWithConditionRule.condition is Conditions.IsMasterMode) {
                             if (ItemID.Search.TryGetName(eaterWithConditionRule.itemId, out string name)) {
                                 if (!ServerConfig.Instance.MasterToExpertBlacklist.Contains(new ItemDefinition(name))) {
-                                    eaterWithConditionRule.condition = new Conditions.IsExpert();
+                                    if (ServerConfig.Instance.DropRelicsClassic) {
+                                        eaterWithConditionRule.condition = new Conditions.NotMasterMode();
+                                    }
+
+                                    else if (ServerConfig.Instance.DropRelicsExpert) {
+                                        eaterWithConditionRule.condition = new Conditions.IsExpert();
+                                    }
+
+                                    ArraySystem.RegisterNewRelic(eaterWithConditionRule.itemId);
                                 }
                             }
                         }
-                        if (chain.RuleToChain is DropBasedOnMasterMode eaterMasterDrop && eaterMasterDrop.ruleForMasterMode is DropPerPlayerOnThePlayer eaterDrop && eaterDrop.condition is Conditions.IsMasterMode && ServerConfig.Instance.DropPets) {
+                        if (chain.RuleToChain is DropBasedOnMasterMode eaterMasterDrop && eaterMasterDrop.ruleForMasterMode is DropPerPlayerOnThePlayer eaterDrop && eaterDrop.condition is Conditions.IsMasterMode) {
                             if (ItemID.Search.TryGetName(eaterDrop.itemId, out string name)) {
                                 if (!ServerConfig.Instance.MasterToExpertBlacklist.Contains(new ItemDefinition(name))) {
                                     // Add and remove since the original rule is a DropBasedOnMasterMode drop
                                     int denom = ServerConfig.Instance.GuaranteedPets ? 1 : eaterDrop.chanceDenominator;
-                                    LeadingConditionRule newRule = new LeadingConditionRule(new Conditions.LegacyHack_IsABoss());
-                                    newRule.OnSuccess(new DropPerPlayerOnThePlayer(eaterDrop.itemId, denom, eaterDrop.amountDroppedMinimum, eaterDrop.amountDroppedMaximum, new Conditions.IsExpert()));
-                                    npcLoot.Add(newRule);
-                                    npcLoot.Remove(eaterDrop);
+                                    LeadingConditionRule newRule = new LeadingConditionRule(new Conditions.MissingTwin());
+
+                                    if (ServerConfig.Instance.DropPetsClassic) {
+                                        newRule.OnSuccess(new DropPerPlayerOnThePlayer(eaterDrop.itemId, denom, eaterDrop.amountDroppedMinimum, eaterDrop.amountDroppedMaximum, new Conditions.NotMasterMode()));
+                                        npcLoot.Add(newRule);
+                                        npcLoot.Remove(eaterDrop);
+                                    }
+
+                                    else if (ServerConfig.Instance.DropPetsExpert) {
+                                        newRule.OnSuccess(new DropPerPlayerOnThePlayer(eaterDrop.itemId, denom, eaterDrop.amountDroppedMinimum, eaterDrop.amountDroppedMaximum, new Conditions.IsExpert()));
+                                        npcLoot.Add(newRule);
+                                        npcLoot.Remove(eaterDrop);
+                                    }
+
+                                    ArraySystem.RegisterNewPet(eaterDrop.itemId);
                                 }
                             }
                         }
