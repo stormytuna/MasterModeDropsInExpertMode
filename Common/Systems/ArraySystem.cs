f3eab9ccfb534db5af64946b96ce776986906184
stormytuna <stormytuna@outlook.com>
2022-11-14 09:07:24 +0000

    Update array system to store pets and relics separately

--- Common/Systems/ArraySystem.cs
+++ Common/Systems/ArraySystem.cs
@@ -3,14 +3,24 @@ using Terraria.ModLoader;
 
 namespace MasterModeDropsInExpertMode.Common.Systems {
     public class ArraySystem : ModSystem {
-        public static List<int> NewExpertDrops { get; private set; } = new();
+        public static List<int> Relics { get; private set; } = new();
+        public static List<int> Pets { get; private set; } = new();
 
         public override void Unload() {
-            NewExpertDrops = null;
+            Relics = null;
+            Pets = null;
         }
 
-        public void RegisterNewExpertDrop(int type) {
-            NewExpertDrops.Add(type);
+        public static void RegisterNewRelic(int type) {
+            if (!Relics.Contains(type)) {
+                Relics.Add(type);
+            }
+        }
+
+        public static void RegisterNewPet(int type) {
+            if (!Pets.Contains(type)) {
+                Pets.Add(type);
+            }
         }
     }
 }
