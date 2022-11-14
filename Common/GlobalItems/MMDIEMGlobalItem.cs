426b776bf68c27707590537ac92074b2d8bda536
stormytuna <stormytuna@outlook.com>
2022-11-14 09:08:23 +0000

    Change tooltip based on how they drop

--- Common/GlobalItems/MMDIEMGlobalItem.cs
+++ Common/GlobalItems/MMDIEMGlobalItem.cs
@@ -1,14 +1,23 @@
 ﻿using MasterModeDropsInExpertMode.Common.Configs;
 using MasterModeDropsInExpertMode.Common.Systems;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace MasterModeDropsInExpertMode.Common.GlobalItems {
     public class MMDIEMGlobalItem : GlobalItem {
         public override void SetDefaults(Item item) {
-            if (ArraySystem.NewExpertDrops.Contains(item.type) && ServerConfig.Instance.FixTooltips) {
+            if ((ArraySystem.Relics.Contains(item.type) || ArraySystem.Pets.Contains(item.type)) && ServerConfig.Instance.FixTooltips) {
                 item.master = false;
-                item.expert = true;
+
+                if (ServerConfig.Instance.DropRelicsClassic) {
+                    item.rare = ArraySystem.Relics.Contains(item.type) ? ItemRarityID.Green : ItemRarityID.LightPurple;
+                }
+
+                else if (ServerConfig.Instance.DropRelicsExpert) {
+                    item.expert = true;
+                    item.rare = ItemRarityID.Expert;
+                }
             }
         }
     }
